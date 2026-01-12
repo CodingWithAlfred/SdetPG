f36abe22305f3c38a2e4102405ea8be4eec7450d
Editopia <Editopia@Homecomputer>
2026-01-12 16:54:27 -0500

    chore: setup UI Prometheus job search test and install dependencies added test setting model

---
+++ UIPrometheusJobSearchTest/Models/TestSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIPrometheusJobSearchTest.Models
+{
+    public class TestSettings
+    {
+        public string BaseUrl { get; set; } = string.Empty;
+        public string PrometheusSearchTerm { get; set; } = string.Empty;
+        public string ExpectedJobTitle { get; set; } = string.Empty;
+        public string BrowserType { get; set; } = "chromium";
+        public bool Headless { get; set; }
+        public int DefaultTimeout { get; set; } = 30000;
+    }
+}
